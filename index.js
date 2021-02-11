fd04f3c5861b7a9b7572e2dde7995dfd656afa25
leminhhieu98py@gmail.com <leminhhieu98py@gmail.com>
2021-02-11 20:32:41 +0700

    responsive for mobile

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+// auto slideshow
 $(".carousel").carousel();
 $(".carousel").carousel({
   interval: 10,
 });
 $(".carousel").carousel("cycle");
+
+// event when click on side-nav button
+let menuBtn = document.getElementById("nav-btn");
+let sideNav = document.getElementById("sideNav");
+sideNav.style.left = "-200px";
+menuBtn.addEventListener("click", () => {
+  if (sideNav.style.left == "-200px") {
+    sideNav.style.left = "0";
+  } else {
+    sideNav.style.left = "-200px";
+  }
+});
